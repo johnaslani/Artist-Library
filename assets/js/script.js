bf612d1cdb7e109d17eeb2f37581100b7c8e51cc
Ryan Whitmore <ryan.whit@outlook.com>
2022-07-30 13:24:36 -0700

    Added bulma styling to the card content. Added card to field of pre-populated cards.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,55 @@
+// Globals
+var NAPSTER_API_KEY = "ZTE3MmM4YTItMzY0Ni00OGM1LWE2NDUtMDc2ZDgyNGUwMGQz"
+
+
+// =============================================================================
+// DOM Objects
 var searchTerm = document.querySelector("#search-input");
 var resultTextEl = document.querySelector('#result-text');
-var resultContentEl = document.querySelector('#result-content');
+// var resultContentEl = document.querySelector('#result-content');
 var searchFormEl = document.querySelector('#search-form');
 var formatEl = document.querySelector('#format-input');
+var searchResultsEl = document.querySelector('#search-results');
 
 // =============================================================================
 // Napster API Fetch Calls
-var API_KEY = "ZTE3MmM4YTItMzY0Ni00OGM1LWE2NDUtMDc2ZDgyNGUwMGQz"
 
+// Generate the DOM elements for an artist card
+function generateArtistCard(artistContents) {
+  var card = $("<div>")
+  card.addClass("card")
+
+  // Generate the header
+  var header = $("<header>")
+  header.addClass("card-header")
+  var headerTitle = $("<p>")
+  headerTitle.addClass("card-header-title")
+  headerTitle.text(artistContents.name)
+  header.append(headerTitle)
+  card.append(header)
+
+  // Card content
+  var cardContent = $("<div>")
+  cardContent.addClass("card-content")
+  var content = $("<div>")
+  content.addClass("content")
+  content.text(artistContents.bio)
+  cardContent.append(content)
+  card.append(cardContent)
+
+  console.log(card);
+
+  return card
+}
+
+// Artist Search
 function searchNapsterArtist(artistSearchInput) {
     
   var artistSearchUrl = (
     'https://api.napster.com/v2.2/search?query='
     + artistSearchInput.split(' ').join('%20')
-    + '&type=artist&per_type_limit=1&apikey='
-    + API_KEY
+    + '&type=artist&per_type_limit=3&apikey='
+    + NAPSTER_API_KEY
   );
 
   fetch(artistSearchUrl)
@@ -29,14 +64,155 @@ function searchNapsterArtist(artistSearchInput) {
 
       var data = responseData.search.data;
       var artistData = data.artists[0];
-      var artistName = artistData.name;
-      var artistId = artistData.id;
 
       // Album IDs
-      var topAlbums = artistData.albumGroups.main.slice(0, 5);
+      // var topAlbums = artistData.albumGroups.main.slice(0, 5);
+      
+      var artistContents = {
+        name: artistData.name,
+        bio: artistData.bios[0].bio,
+        id: artistData.id,
+        imagesRef: artistData.links.images.href,
+      };
 
-      console.log("name: ", artistName);
-      console.log("id: ", artistId);
+      // var domCardContents = generateArtistCard(artistContents)
+      var card = $("<div>");
+      card.addClass("card");
+
+      // Generate the header
+      var header = $("<header>");
+      header.addClass("card-header");
+      var headerTitle = $("<p>");
+      headerTitle.addClass("card-header-title");
+      headerTitle.text(artistContents.name);
+      header.append(headerTitle);
+      card.append(header);
+
+      // Card content
+      var cardContent = $("<div>");
+      cardContent.addClass("card-content");
+      var content = $("<div>");
+      content.addClass("content");
+      content.text(artistContents.bio);
+      cardContent.append(content);
+      card.append(cardContent);
+
+      // Footer
+      var footerEl = $("<footer>");
+      footerEl.addClass("card-footer");
+      var saveEl = $("<a>");
+      saveEl.addClass("card-footer-item").text("Save")
+      saveEl.attr("#")
+      footerEl.append(saveEl)
+      
+      var moreInfoEl = $("<a>");
+      moreInfoEl.addClass("card-footer-item").text("More Info")
+      moreInfoEl.attr("#")
+      footerEl.append(moreInfoEl)
+      card.append(footerEl)
+
+      card.appendTo(searchResultsEl);
+      // searchResultsEl.append(card)
+
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+}
+
+  // Image Search
+function searchArtistImages(albumSearchInput) {
+    
+  var albumSearchUrl = (
+    "https://api.napster.com/v2.2/artists/art.15352652/images"
+    + '?apikey='
+    + NAPSTER_API_KEY
+  );
+
+  fetch(albumSearchUrl)
+    .then(function (response) {
+      if (!response.ok) {
+        throw response.json();
+      }
+      return response.json();
+    })
+    .then(function (responseData) {
+      console.log(responseData);
+
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+  }
+
+
+// Album Search
+function searchNapsterAlbum(albumSearchInput) {
+    
+  var albumSearchUrl = (
+    'https://api.napster.com/v2.2/search?query='
+    + albumSearchInput.split(' ').join('%20')
+    + '&type=album&per_type_limit=1&apikey='
+    + NAPSTER_API_KEY
+  );
+
+  fetch(albumSearchUrl)
+    .then(function (response) {
+      if (!response.ok) {
+        throw response.json();
+      }
+      return response.json();
+    })
+    .then(function (responseData) {
+      console.log(responseData);
+
+      // var data = responseData.search.data;
+      // var artistData = data.artists[0];
+      // var artistName = artistData.name;
+      // var artistId = artistData.id;
+
+      // // Album IDs
+      // var topAlbums = artistData.albumGroups.main.slice(0, 5);
+
+      // console.log("name: ", artistName);
+      // console.log("id: ", artistId);
+
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+  }
+
+// Track Seach
+function searchNapsterTrack(trackSearchInput) {
+    
+  var trackSearchUrl = (
+    'https://api.napster.com/v2.2/search?query='
+    + trackSearchInput.split(' ').join('%20')
+    + '&type=track&per_type_limit=1&apikey='
+    + NAPSTER_API_KEY
+  );
+
+  fetch(trackSearchUrl)
+    .then(function (response) {
+      if (!response.ok) {
+        throw response.json();
+      }
+      return response.json();
+    })
+    .then(function (responseData) {
+      console.log(responseData);
+
+      // var data = responseData.search.data;
+      // var artistData = data.artists[0];
+      // var artistName = artistData.name;
+      // var artistId = artistData.id;
+
+      // // Album IDs
+      // var topAlbums = artistData.albumGroups.main.slice(0, 5);
+
+      // console.log("name: ", artistName);
+      // console.log("id: ", artistId);
 
     })
     .catch(function (error) {
@@ -86,6 +262,8 @@ const options = {
 // add the Video Api -
 
 
+// =============================================================================
+// Capture user search input and make appropriate API calls
 function handleSearchFormSubmit(event) {
   event.preventDefault();
 
@@ -108,7 +286,7 @@ function handleSearchFormSubmit(event) {
       console.log("Calling Napster API for album search: ", formatInputVal)
   }
   if (formatInputVal === "track") {
-      searchNapsterArtist(searchInputVal);
+      searchNapsterTrack(searchInputVal);
       console.log("Calling Napster API for track search: ", formatInputVal)
   }
 
@@ -116,4 +294,6 @@ function handleSearchFormSubmit(event) {
   
 
 searchFormEl.addEventListener('submit', handleSearchFormSubmit);
-// getParams();
+
+// TODO: Add another search input field after the Napster call is returned
+//    ... or maybe add another search box on the napster return?
